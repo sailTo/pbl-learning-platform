2f8523823038e251291b8a9c1efd04da830571de
fudanwqd <17302010058@fudan.edu.cn>
2020-05-21 19:58:20 +0800

    Merge branch 'master' of G:\大三\大三下\高级web\lab\pj\pbl-learning-platform-master\pbl-learning-platform with conflicts.

--- supernova/src/app/pages/home/home.module.ts
+++ supernova/src/app/pages/home/home.module.ts
@@ -3,22 +3,21 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
-import {DescriptionBorderComponent} from './description-border/description-border.component';
+import{DescriptionBorderComponent} from './description-border/description-border.component'
 import { NgZorroAntdModule} from 'ng-zorro-antd';
 import {AvatarComponent} from './avatar/avatar.component';
-// import { TableEditRowComponent } from './table-edit-row/table-edit-row.component';
 import {FormsModule} from '@angular/forms';
-import { CourseCardComponent } from './course-card/course-card.component';
+import { CourseCardComponent } from './course-card/course-card.component'
 
 @NgModule({
-  declarations: [HomeComponent, DescriptionBorderComponent, AvatarComponent, CourseCardComponent],
+  declarations: [HomeComponent,DescriptionBorderComponent,AvatarComponent, CourseCardComponent],
   imports: [
     CommonModule,
     HomeRoutingModule,
     NgZorroAntdModule,
     FormsModule
   ],
-  providers: [
+  providers:[
     AvatarComponent
   ]
 })
