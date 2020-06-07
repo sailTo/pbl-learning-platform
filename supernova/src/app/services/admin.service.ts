62fd12c6f926f348dc29dbed0abb090923fa3b2c
fudanwqd <17302010058@fudan.edu.cn>
2020-06-08 01:00:47 +0800

    个人界面部分修改 完成用户管理页面，注册页面，登录页面    -wqd

--- supernova/src/app/services/admin.service.ts
+++ supernova/src/app/services/admin.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import {User} from '../models/user';
-import { HttpClient,HttpParams } from '@angular/common/http';
+import { HttpClient,HttpParams,HttpHeaders } from '@angular/common/http';
 import {environment} from '../../environments/environment';
+import {Response} from '../models/generic-response';
+import {FormGroup} from '@angular/forms';
 @Injectable({
   providedIn: 'root'
 })
@@ -10,13 +12,46 @@ export class AdminService {
   constructor(
     private http:HttpClient,
   ) { }
+  getUserInformation(){
+    const params = new HttpParams({fromObject:{
+      pbl_token: String(JSON.parse(localStorage.getItem("User")).token),
+    }})
+    return this.http.get<Response<{users: User[],images:string[]}>>(`${environment.apiUrl}/api/searchAllUsers`,{params});
+  }
+
   updateInformation(user:User){
     const params = new HttpParams({fromObject:{
       pbl_token: String(JSON.parse(localStorage.getItem("User")).token),
-      content: JSON.stringify(user),
+      user: JSON.stringify(user),
       
     }})
-    return this.http.put<any>(`${environment.apiUrl}/api/changeInformation`,{params});
+    return this.http.put<Response<{}>>(`${environment.apiUrl}/api/changeInformation`,params);
   }
+  addNewUser(user:User){
+    let headers = {
+      headers: new HttpHeaders({
+        'Content-Type': "application/x-www-form-urlencoded;charset=UTF-8"
+      })
+    }
+    const params = {
+      pbl_token: String(JSON.parse(localStorage.getItem("User")).token),
+      user:JSON.stringify(user),
+    };
+    return this.http.post<Response<{}>>(`${environment.apiUrl}/api/addUser`,this.transformRequest(params),headers) 
+  }
+  transformRequest(data) {
 
+            var str = '';
+    
+            for (var i in data) {
+    
+              str += i + '=' + data[i] + '&';
+    
+            }
+    
+            str.substring(0, str.length - 1);
+    
+            return str;
+    
+      };
 }
