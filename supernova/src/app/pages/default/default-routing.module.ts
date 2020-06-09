b0a18ada733f3d9315985e9c67be1edf0eae12f9
HNoodles <13671980486@163.com>
2020-06-09 23:18:44 +0800

    updated routes

--- supernova/src/app/pages/default/default-routing.module.ts
+++ supernova/src/app/pages/default/default-routing.module.ts
@@ -2,28 +2,75 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DefaultComponent } from './default.component';
 
-
 const routes: Routes = [
   {
     path: '',
     component: DefaultComponent,
     children: [
       { path: '', pathMatch: 'full', redirectTo: 'courses' },
-      { path: 'welcome', loadChildren: () => import('../welcome/welcome.module').then(m => m.WelcomeModule) },
-      { path: 'home', loadChildren: () => import('../home/home.module').then(m => m.HomeModule) },
-      { path: 'courses', loadChildren: () => import('../courses/courses.module').then(m => m.CoursesModule) },
-      { path: 'score', loadChildren: () => import('../score/score.module').then(m => m.ScoreModule) },
-      { path: 'signup', loadChildren: () => import('../signup/signup.module').then(m => m.SignupModule) },
-      { path: 'projects', loadChildren: () => import('../projects/projects.module').then(m => m.ProjectsModule) },
-      { path: 'project', loadChildren: () => import('../project-details/project-details.module').then(m => m.ProjectDetailsModule) },
-      { path: 'admin', loadChildren: () => import('../admin/admin.module').then(m => m.AdminModule) },
-    ]
-  }
+      {
+        path: 'welcome',
+        loadChildren: () =>
+          import('../welcome/welcome.module').then((m) => m.WelcomeModule),
+      },
+      {
+        path: 'home',
+        loadChildren: () =>
+          import('../home/home.module').then((m) => m.HomeModule),
+      },
+      {
+        path: 'courses',
+        loadChildren: () =>
+          import('../courses/courses.module').then((m) => m.CoursesModule),
+      },
+      {
+        path: 'score',
+        loadChildren: () =>
+          import('../score/score.module').then((m) => m.ScoreModule),
+      },
+      {
+        path: 'signup',
+        loadChildren: () =>
+          import('../signup/signup.module').then((m) => m.SignupModule),
+      },
+      {
+        path: 'projects',
+        loadChildren: () =>
+          import('../projects/projects.module').then((m) => m.ProjectsModule),
+      },
+      {
+        path: 'project',
+        loadChildren: () =>
+          import('../project-details/project-details.module').then(
+            (m) => m.ProjectDetailsModule
+          ),
+      },
+      {
+        path: 'admin',
+        loadChildren: () =>
+          import('../admin/admin.module').then((m) => m.AdminModule),
+      },
 
+      {
+        path: 'admin_course',
+        loadChildren: () =>
+          import('../admin-course/admin-course.module').then(
+            (m) => m.AdminCourseModule
+          ),
+      },
+      {
+        path: 'admin_project',
+        loadChildren: () =>
+          import('../admin-project/admin-project.module').then(
+            (m) => m.AdminProjectModule
+          ),
+      },
+    ],
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class DefaultRoutingModule { }
