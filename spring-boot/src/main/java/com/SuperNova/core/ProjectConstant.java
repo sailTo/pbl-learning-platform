1f4536baff5c6f386ecae0908e9bb455c1a34492
FDChongli <17302010025@fudan.edu.cn>
2020-06-10 19:48:19 +0800

    修改文件基础地址，测试时忘记修改回来了

--- spring-boot/src/main/java/com/SuperNova/core/ProjectConstant.java
+++ spring-boot/src/main/java/com/SuperNova/core/ProjectConstant.java
@@ -7,7 +7,7 @@ public final class ProjectConstant {
     public static final String BASE_PACKAGE = "com.SuperNova";//生成代码所在的基础包名称，可根据自己公司的项目修改（注意：这个配置修改之后需要手工修改src目录项目默认的包路径，使其保持一致，不然会找不到类）
 
     public static final String IMG_BASE = "/www/wwwroot/www.zhsyy.top/SuperNova/UploadImage/";//上传图片根地址，部署前记得修改
-    public static final String File_BASE = "C:\\UploadFile\\";//上传文件根地址，部署前记得修改
+    public static final String File_BASE = "/www/wwwroot/www.zhsyy.top/SuperNova/UploadFile/";//上传文件根地址，部署前记得修改
     public static final String WEB_IMG_BASE = "http://123.56.219.88/SuperNova/UploadImage/";//前端下载图片根地址，部署前记得修改
     public static final String WEB_File_BASE = "http://123.56.219.88/SuperNova/UploadFile/";//前端下载文件根地址，部署前记得修改
 
