a13ec978b1144ec6d71a77afc005ec0426f29e33
HNoodles <13671980486@163.com>
2020-06-20 17:40:00 +0800

    modified style of admin course buttons

--- supernova/src/app/pages/admin-course/admin-course.module.ts
+++ supernova/src/app/pages/admin-course/admin-course.module.ts
@@ -7,18 +7,18 @@ import { AdminCourseComponent } from './admin-course.component';
 
 import { NzAvatarModule } from 'ng-zorro-antd/avatar';
 import { NzCardModule } from 'ng-zorro-antd/card';
-import { NzTabsModule } from "ng-zorro-antd/tabs";
+import { NzTabsModule } from 'ng-zorro-antd/tabs';
 import { NzIconModule } from 'ng-zorro-antd/icon';
-import { NzGridModule } from "ng-zorro-antd/grid";
+import { NzGridModule } from 'ng-zorro-antd/grid';
 import { NzPaginationModule } from 'ng-zorro-antd/pagination';
 import { NzBadgeModule } from 'ng-zorro-antd/badge';
 import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NzTagModule } from 'ng-zorro-antd/tag';
 import { NzStatisticModule } from 'ng-zorro-antd/statistic';
 import { NzModalModule } from 'ng-zorro-antd/modal';
-import { NzMessageModule } from "ng-zorro-antd/message";
+import { NzMessageModule } from 'ng-zorro-antd/message';
 import { NzFormModule } from 'ng-zorro-antd/form';
-import { NzInputModule } from "ng-zorro-antd/input";
+import { NzInputModule } from 'ng-zorro-antd/input';
 import { NzInputNumberModule } from 'ng-zorro-antd/input-number';
 import { NzUploadModule } from 'ng-zorro-antd/upload';
 import { NzTableModule } from 'ng-zorro-antd/table';
@@ -26,12 +26,11 @@ import { NzAffixModule } from 'ng-zorro-antd/affix';
 import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
 import { NzSelectModule } from 'ng-zorro-antd/select';
 import { CoursesModule } from '../courses/courses.module';
-import {NzRadioModule} from 'ng-zorro-antd/radio';
+import { NzRadioModule } from 'ng-zorro-antd/radio';
+import { NzDividerModule } from 'ng-zorro-antd/divider';
 
 @NgModule({
-  declarations: [
-    AdminCourseComponent
-  ],
+  declarations: [AdminCourseComponent],
   imports: [
     CommonModule,
     AdminCourseRoutingModule,
@@ -57,7 +56,8 @@ import {NzRadioModule} from 'ng-zorro-antd/radio';
     NzAffixModule,
     NzDropDownModule,
     NzSelectModule,
-    NzRadioModule
-  ]
+    NzRadioModule,
+    NzDividerModule,
+  ],
 })
-export class AdminCourseModule { }
+export class AdminCourseModule {}
