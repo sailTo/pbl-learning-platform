7586f6d1918f075cba649791a25b45ce1840d0e3
Zhsyy <709995380@qq.com>
2020-06-20 21:39:18 +0800

    完成project和course页面

--- supernova/src/environments/environment.ts
+++ supernova/src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  // apiUrl: 'http://123.56.219.88:8081',
-  apiUrl: 'http://127.0.0.1:8081',
+  apiUrl: 'http://123.56.219.88:8081',
+  // apiUrl: 'http://127.0.0.1:8081',
   jwtHeader: 'supernova',
   defaultImgPath: 'http://123.56.219.88/UploadImage/default.img',
   deleteU_id: '0',
