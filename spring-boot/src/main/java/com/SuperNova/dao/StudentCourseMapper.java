7678fe00cda0deca9184b72452850f011f6a6c03
Sailing <13625949+sailTo@users.noreply.github.com>
2021-12-29 22:39:11 +0800

    remove old codes

--- spring-boot/src/main/java/com/SuperNova/dao/StudentCourseMapper.java
+++
@@ -1,7 +0,0 @@
-package com.SuperNova.dao;
-
-import com.SuperNova.core.Mapper;
-import com.SuperNova.model.StudentCourse;
-
-public interface StudentCourseMapper extends Mapper<StudentCourse> {
-}
--- spring-boot/src/main/java/com/SuperNova/dao/StudentGradeMapper.java
+++
@@ -1,9 +0,0 @@
-package com.SuperNova.dao;
-
-import com.SuperNova.core.Mapper;
-import com.SuperNova.model.StudentGrade;
-
-import java.util.List;
-
-public interface StudentGradeMapper extends Mapper<StudentGrade> {
-}
